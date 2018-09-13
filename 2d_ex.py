4c0e4b4becb5cea023fe21c9af9fc6934a0414d6
Zegs <sergeyzub54568@gmail.com>
2018-09-13 20:16:22 +0300

    edit task

--- 2d_ex.py
+++ 2d_ex.py
@@ -1,33 +1,35 @@
 # lab №1 ex. 2 | Var 9 Sergey Zubrilin 9/13/18
 
-def is_prime(digit):
-    if digit is None:
+
+def is_prime(number):
+    count = 0
+    if number is None:
         print('Error -- wrong format data')
-
-    if digit == 0:
-        return False
-
-    for i in (2, digit-1):
-        if digit % i == 0:
+    for i in range(number):
+        if number % (i+1) == 0:
+            count += 1
+            print(count)
+        if count > 2:
             return False
 
     return True
 
 
-def first_odd(population=1011474):
-    for el in str(population)[::-1]:
+def first_odd(population=1011494):
+    reverse_population = str(population)[::-1]
+    for i, el in enumerate(reverse_population):
         if int(el) % 2 != 0:
-            return int(el)
+            return int(reverse_population[i:][::-1])
     return None
 
 
 def ex2():
     f_odd = first_odd()
-    print('{0} - first odd element'.format(f_odd))
+    print('{0} - new number'.format(f_odd))
     if is_prime(f_odd):
         print('{0} is prime'.format(f_odd))
     else:
-        print('Ell {0} is not prime'.format(f_odd))
+        print('El ({0}) is not prime'.format(f_odd))
     return None
 
 

672a8cc7ebae9adb44f52f04051fbeb51c44b05c
Zegs <sergeyzub54568@gmail.com>
2018-09-24 16:31:16 +0300

    comleat ex5 and make unit test

--- ex5.py
+++ ex5.py
@@ -4,10 +4,10 @@
 from tools import sign_init
 
 
-class DigitByFloat:
+class MyFloat:
     def __init__(self, sign='', mantissa=1, exponent=1):
         if sign != '' and sign != '-':
-            raise AttributeError("wrong format data, attr 'sign'")
+                raise AttributeError("wrong format data, attr 'sign'")
         if not isinstance(mantissa, int):
             raise AttributeError("wrong format data, attr 'mantissa'")
         if not isinstance(exponent, int):
@@ -29,25 +29,31 @@ class DigitByFloat:
         sign = sign_init(self.sign)
         return float(self.mantissa)*(10 ** int(self.exponent - len(self.mantissa)))*sign
 
-    # @classmethod
-    # def convert_from_float(cls, fl):
-    #     str_fl = str(fl)
-    #     sign = ''
-    #     exponent = 1
-    #
-    #     if str_fl[0] == '-':
-    #         sign = '-'
-    #         str_fl = str_fl[1:]
-    #
-    #     if '+' in str_fl:
-    #         exponent = int(str_fl[str_fl.find('+') + 1:])
-    #         mantissa = int(str_fl[:str_fl.find('+')-1].replace('.', ''))
-    #     else:
-    #         mantissa = int(str_fl.replace('.', ''))
-    #
-    #     return cls(sign, mantissa, exponent)
+    @classmethod
+    def convert_from_float(cls, fl):
+        str_fl = str(fl)
+        sign = ''
+        exponent = 0
+
+        if str_fl[0] == '-':
+            sign = '-'
+            str_fl = str_fl[1:]
+        if '+' in str_fl:
+            exponent = int(str_fl[str_fl.find('+') + 1:]) + 1
+            mantissa = int(str_fl[:str_fl.find('+')-1].replace('.', ''))
+        else:
+            mantissa = int(str_fl.replace('.', ''))
+            if mantissa != 0:
+                exponent = len(str(str_fl)) - 3
+
+        return cls(sign, mantissa, exponent)
 
     def __str__(self):
+        if self.mantissa == '0':
+            return f'{self.sign}0.0'
+        return f'{self.sign}0.{self.mantissa}E+{self.exponent}'
+
+    def __repr__(self):
         if self.mantissa == '0':
             return f'{self.sign}0.0'
         return f'{self.sign}0.{self.mantissa}E+{self.exponent}'
@@ -95,22 +101,20 @@ class DigitByFloat:
         return False
 
     def __sub__(self, other):
+        """Я написал два метода под конвертацию в классический float и обратно, т.к. многие арифметические операции
+        требовали частичной конвертации в том или ином виде."""
+
         fl1 = self.convert_to_float()
         fl2 = other.convert_to_float()
         fl = fl1 - fl2
-        # res = self.convert_from_float(fl)
+        res = self.convert_from_float(fl)
 
-        # print(f'{fl} - test 1 with out convert')
-        return fl
+        return res
 
+    def __ne__(self, other):
+        if self.sign != other.sign or self.exponent != other.exponent or self.mantissa != other.mantissa:
+            return True
+        else:
+            return False
 
-fl1 = DigitByFloat(sign='-', mantissa=12341, exponent=17)
-fl2 = DigitByFloat(sign='-', mantissa=123, exponent=17)
-print(fl1)
-# print(fl1.convert_to_float())
-
-print(fl2)
-
-fl3 = fl1 - fl2
-print(fl3)
 

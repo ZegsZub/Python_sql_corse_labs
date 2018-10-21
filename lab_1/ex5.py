26df577410989310f08ae444c63a26ce0b3fe2f1
Zegs <sergeyzub54568@gmail.com>
2018-10-21 20:10:38 +0300

    rebuild MyFloat

--- lab_1/ex5.py
+++ lab_1/ex5.py
@@ -14,44 +14,8 @@ class MyFloat:
             raise AttributeError("wrong format data, attr 'mantissa'")
 
         self.sign = sign
-        self.mantissa = str(mantissa)
-
-        if self.mantissa[0] != '0':
-            self.exponent = exponent
-        else:
-            self.exponent = 0
-
-        while self.mantissa[-1] == '0' and len(self.mantissa) > 1:
-            self.mantissa = self.mantissa[:-1]
-            self.exponent += 1
-
-    def convert_to_float(self):
-        sign = sign_init(self.sign)
-        return float(self.mantissa)*(10 ** int(self.exponent - len(self.mantissa)))*sign
-
-    @classmethod
-    def convert_from_float(cls, fl):
-        str_fl = str(fl)
-        sign = ''
-        exponent = 0
-
-        if str_fl[0] == '-':
-            sign = '-'
-            str_fl = str_fl[1:]
-        if '+' in str_fl:
-            exponent = int(str_fl[str_fl.find('+') + 1:]) + 1
-            mantissa = int(str_fl[:str_fl.find('+')-1].replace('.', ''))
-        else:
-            mantissa = int(str_fl.replace('.', ''))
-            if mantissa != 0:
-                exponent = len(str(str_fl)) - 3
-
-        return cls(sign, mantissa, exponent)
-
-    def __str__(self):
-        if self.mantissa == '0':
-            return f'{self.sign}0.0'
-        return f'{self.sign}0.{self.mantissa}E+{self.exponent}'
+        self.mantissa = mantissa
+        self.exponent = exponent
 
     def __repr__(self):
         if self.mantissa == '0':
@@ -59,62 +23,77 @@ class MyFloat:
         return f'{self.sign}0.{self.mantissa}E+{self.exponent}'
 
     def __gt__(self, other):
-        sign1 = sign_init(self.sign)
-        sign2 = sign_init(other.sign)
-
-        if sign1 > sign2:
+        if sign_init(self.sign) > sign_init(other.sign):
             return True
-
-        if sign1 == sign2 == 1:
-            if self.exponent > other.exponent:
-                return True
-            if self.mantissa > other.mantissa:
-                return True
-
-        if sign1 == sign2 == -1:
-            if self.exponent < other.exponent:
-                return True
-            if self.mantissa < other.mantissa:
-                return True
-
+        if self.exponent > other.exponent:
+            return True
+        if self.mantissa > other.mantissa:
+            return True
         return False
 
     def __ge__(self, other):
-        sign1 = sign_init(self.sign)
-        sign2 = sign_init(other.sign)
-
-        if sign1 > sign2:
+        if sign_init(self.sign) > sign_init(other.sign):
             return True
-
-        if sign1 == sign2 == 1:
-            if self.exponent >= other.exponent:
-                return True
-            if self.mantissa >= other.mantissa:
-                return True
-
-        if sign1 == sign2 == -1:
-            if self.exponent <= other.exponent:
-                return True
-            if self.mantissa <= other.mantissa:
-                return True
-
+        if self.exponent > other.exponent:
+            return True
+        if self.mantissa > other.mantissa:
+            return True
+        if (self.mantissa == other.mantissa) and (self.exponent == other.exponent)\
+                                            and (sign_init(self.sign) == sign_init(other.sign)):
+            return True
         return False
 
-    def __sub__(self, other):
-        """Я написал два метода под конвертацию в классический float и обратно, т.к. многие арифметические операции
-        требовали частичной конвертации в том или ином виде."""
-
-        fl1 = self.convert_to_float()
-        fl2 = other.convert_to_float()
-        fl = fl1 - fl2
-        res = self.convert_from_float(fl)
-
-        return res
-
     def __ne__(self, other):
-        if self.sign != other.sign or self.exponent != other.exponent or self.mantissa != other.mantissa:
+        if (self.mantissa != other.mantissa) or (self.exponent != other.exponent)\
+                                            or (sign_init(self.sign) != sign_init(other.sign)):
             return True
-        else:
-            return False
+        return False
+
+    @staticmethod
+    def to_int(my_float):
+        return sign_init(my_float.sign)*my_float.mantissa*(10**(my_float.exponent-len(str(my_float.mantissa))))
+
+    @staticmethod
+    def my_round(number, ndigits):
+
+        s = str(number)
+        sign = 1
+        if '-' in s:
+            s = s.replace('-', '')
+            sign = -1
+
+        if len(s) > ndigits:
+
+            for i in range(ndigits, len(s)):
+                if int(s[i]) >= 4:
+                    if int(s[i]) >= 5:
+                        return f'{(int(s[:ndigits])+1) * sign}'+'0'*(len(s)-ndigits)
+                    else:
+                        continue
+                else:
+                    return f'{int(s[:ndigits])* sign}'+'0'*(len(s)-ndigits)
+
+        return str(number)
+
+    def __sub__(self, other):
+        res_exponent = 0
+        res_sign = ''
+        int_value = int(self.my_round(self.to_int(self) - self.to_int(other),
+                                  max(len(str(self.mantissa)), len(str(other.mantissa)))))
+        str_value = str(int_value)
+
+        if int_value < 0:
+            res_sign = '-'
+
+        for i in range(str_value.count('0')):
+            if str_value[-(i+1)] == '0':
+                res_exponent += 1
+            else:
+                break
+
+        res_mantissa = int(str_value[:len(str_value)-res_exponent].replace('-', ''))
+        res_exponent += len(str(res_mantissa))
+        return MyFloat(res_sign, res_mantissa, res_exponent)
+
 
 
